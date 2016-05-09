ecf3edd2bca357a065b913373700efedf567774d
Adriaan Meijer <a.meijer@ccvshop.nl>
2016-05-09 18:02:50 +0200

    Verwerken van CodeBlock element bij een install van de App. Maken van SendService Example

--- Examples/SendService/Endpoint.php
+++ Examples/SendService/Endpoint.php
@@ -2,26 +2,28 @@
 
 	$oObject = json_decode(@file_get_contents('php://input'));
 
-	if($oObject instanceof stdClass && isset($oObject->form_data) && !empty($oObject->form_data->type_bacon_text)) {
-
-		$oResponse                      = new \stdClass();
-		$oResponse->view                = 'succes';
-		$oResponse->data                = [];
-		$oResponse->data['description'] = '[View: succes]. Package label has been created succesfully.';
-		$oResponse->data['label-1']     = 'https://ameijer-app.ccvdev.nl/Examples/SendService/specimen_label.png';
-
+	if($oObject instanceof stdClass && isset($oObject->form_data) && !empty($oObject->form_data->submit)) {
+		$oResponse = new \stdClass();
+		if($oObject->form_data->direct_download == '1') {
+			$oResponse->view                     = 'succes-direct-download';
+			$oResponse->data                     = [];
+			$oResponse->data['description']      = '[View: succes-direct-download]. Package label has been created succesfully.';
+			$oResponse->data['package_label_1']  = 'https://ameijer-app.ccvdev.nl/Examples/SendService/specimen_label.png';
+			$oResponse->data['download_label_1'] = 'https://ameijer-app.ccvdev.nl/Examples/SendService/specimen_label.png';
+		} else {
+			$oResponse->view                    = 'succes';
+			$oResponse->data                    = [];
+			$oResponse->data['description']     = '[View: succes]. Package label has been created succesfully.';
+			$oResponse->data['package_label_1'] = 'https://ameijer-app.ccvdev.nl/Examples/SendService/specimen_label.png';
+		}
 		echo json_encode($oResponse, JSON_PRETTY_PRINT);
 		die();
 	}
 
-		$oResponse                              = new \stdClass();
-		$oResponse->view                        = 'onload';
-		$oResponse->data                        = [];
-		$oResponse->data['description']         = '[View: onload]';
-		$oResponse->data['type_bacon_text']     = 'Hello Bacon';
-		$oResponse->data['type_bacon_textarea'] = 'This is the onload text, from a remote source2';
-		$oResponse->data['invoice']             = 'https://ameijer-app.ccvdev.nl/Examples/SendService/specimen_label.png';
-		$oResponse->data['invoice-2']           = 'https://ameijer-app.ccvdev.nl/Examples/SendService/specimen_label.png';
+	$oResponse                      = new \stdClass();
+	$oResponse->view                = 'onload';
+	$oResponse->data                = [];
+	$oResponse->data['description'] = '[View: onload]';
 
-		echo json_encode($oResponse, JSON_PRETTY_PRINT);
+	echo json_encode($oResponse, JSON_PRETTY_PRINT);
 
