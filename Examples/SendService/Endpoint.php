2ad8eeb0d11d41c9da45c677a3a330a6e2a3fe9d
Adriaan Meijer <a.meijer@ccvshop.nl>
2016-05-18 18:30:35 +0200

    Opschonen output

--- Examples/SendService/Endpoint.php
+++ Examples/SendService/Endpoint.php
@@ -5,9 +5,9 @@
 	if($oObject instanceof stdClass && isset($oObject->form_data) && !empty($oObject->form_data->submit)) {
 		$oResponse = new \stdClass();
 		if($oObject->form_data->direct_download == '1') {
-			$oResponse->view                     = 'succes-direct-download';
-			$oResponse->data                     = [];
-			$oResponse->data['package_label_1']  = 'https://ameijer-app.ccvdev.nl/Examples/SendService/Download.php?file=specimen_label.png';
+			$oResponse->view                       = 'succes-direct-download';
+			$oResponse->data                       = [];
+			$oResponse->data['package_label_1']    = 'https://ameijer-app.ccvdev.nl/Examples/SendService/Download.php?file=specimen_label.png';
 			$oResponse->data['attachment_label_1'] = 'https://ameijer-app.ccvdev.nl/Examples/SendService/Download.php?file=specimen_label.png';
 		} else {
 			$oResponse->view                    = 'succes';
@@ -18,10 +18,7 @@
 		die();
 	}
 
-	$oResponse                      = new \stdClass();
-	$oResponse->view                = 'onload';
-	$oResponse->data                = [];
-	$oResponse->data['description'] = '';
-
+	$oResponse       = new \stdClass();
+	$oResponse->view = 'onload';
 	echo json_encode($oResponse, JSON_PRETTY_PRINT);
 
