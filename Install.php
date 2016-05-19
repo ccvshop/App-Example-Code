dd5f0e96e3ea800ba353bd07be18f612e7182dfc
Adriaan Meijer <a.meijer@ccvshop.nl>
2016-05-19 13:10:29 +0200

    Minor grafische opsmuk

--- Install.php
+++ Install.php
@@ -29,82 +29,134 @@
 		<html>
 		<head>
 			<title></title>
+			<!-- Latest compiled and minified CSS -->
+			<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"
+				  integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+			<!-- Optional theme -->
+			<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css"
+				  integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
 		</head>
 		<body>
 		<h1>App Simulator - Installation</h1>
 
 		<p>
 			This is a simulation tool for differnt kind of apps. All apps use the same installation principal, but the effect it will have in the webshop
-			differs. The purpose of this tool is to demo the options and possibilties you could use when developing an app.
+			differs. The purpose of this tool is to demo the options and possibilties you could use when developing an app. The customer id is only for demo
+			purposes.
 		</p>
 
-		<h2>Debug Info</h2>
+		</div>
+		<div class="panel panel-info">
+			<div class="panel-heading">
+				<h3 class="panel-title">
+					Debug Info
+				</h3>
+			</div>
+			<div class="panel-body">
 
-		<p>
-			api_public: <?= $_GET['api_public'] ?>
-		</p>
+				<p>
+					api_public: <?= $_GET['api_public'] ?>
+				</p>
 
-		<h2>Bare installation</h2>
+			</div>
+		</div>
+		<div class="panel panel-default">
+			<div class="panel-heading">
+				<h3 class="panel-title">
+					Bare installation
+				</h3>
+			</div>
+			<div class="panel-body">
 
-		<p>This will only install the app. No futher API calls will be done. This is a good starting point if you have full understanding of the API</p>
+				<p>This will only install the app. No futher API calls will be done. This is a good starting point if you have full understanding of the
+				   API</p>
 
-		<form action="Install.php" method="post">
-			<input type="hidden" name="api_public" id="api_public" value="<?= $_GET['api_public'] ?>"/>
-			<input type="hidden" name="install_type" id="install_type" value="bare"/>
+				<form action="Install.php" method="post">
+					<input type="hidden" name="api_public" id="api_public" value="<?= $_GET['api_public'] ?>"/>
+					<input type="hidden" name="install_type" id="install_type" value="bare"/>
 
-			<label for="customer_id">Customer Id</label>
-			<input type="text" name="customer_id" id="customer_id" value="1337"/>
+					<label for="customer_id">Customer Id</label>
+					<input type="text" name="customer_id" id="customer_id" value="1337"/>
 
-			<button name="Cancel">Cancel</button>
-			<button name="Install">Install</button>
-		</form>
+					<button name="Cancel" class="btn">Cancel</button>
+					<button name="Install" class="btn btn-success">Install</button>
+				</form>
 
-		<h2>Webhook installation</h2>
+			</div>
+		</div>
+		<div class="panel panel-default">
+			<div class="panel-heading">
+				<h3 class="panel-title">
+					Webhook installation
+				</h3>
+			</div>
+			<div class="panel-body">
 
-		<p>This will install the app after creating a few webhooks in the webshop.</p>
+				<p>This will install the app after creating a few webhooks in the webshop.</p>
 
-		<form action="Install.php" method="post">
-			<input type="hidden" name="api_public" id="api_public" value="<?= $_GET['api_public'] ?>"/>
-			<input type="hidden" name="install_type" id="install_type" value="webhooks"/>
+				<form action="Install.php" method="post">
+					<input type="hidden" name="api_public" id="api_public" value="<?= $_GET['api_public'] ?>"/>
+					<input type="hidden" name="install_type" id="install_type" value="webhooks"/>
 
-			<label for="customer_id">Customer Id</label>
-			<input type="text" name="customer_id" id="customer_id" value="1337"/>
+					<label for="customer_id">Customer Id</label>
+					<input type="text" name="customer_id" id="customer_id" value="1337"/>
 
-			<button name="Cancel">Cancel</button>
-			<button name="Install">Install</button>
-		</form>
+					<button name="Cancel" class="btn">Cancel</button>
+					<button name="Install" class="btn btn-success">Install</button>
+				</form>
 
-		<h2>Basic App Code block installation</h2>
+			</div>
+		</div>
+		<div class="panel panel-default">
+			<div class="panel-heading">
+				<h3 class="panel-title">
+					Basic App Code block installation
+				</h3>
+			</div>
+			<div class="panel-body">
 
-		<p>This will install the app after creating an App Code block in the frontend of the webshop. This could be used to place tracking pixels, chat
-		   services, etc. In this example a tracking pixel is added.</p>
+				<p>This will install the app after creating an App Code block in the frontend of the webshop. This could be used to place tracking
+				   pixels, chat
+				   services, etc. In this example a tracking pixel is added.</p>
 
-		<form action="Install.php" method="post">
-			<input type="hidden" name="api_public" id="api_public" value="<?= $_GET['api_public'] ?>"/>
-			<input type="hidden" name="install_type" id="install_type" value="tracking_pixel"/>
+				<form action="Install.php" method="post">
+					<input type="hidden" name="api_public" id="api_public" value="<?= $_GET['api_public'] ?>"/>
+					<input type="hidden" name="install_type" id="install_type" value="tracking_pixel"/>
 
-			<label for="customer_id">Customer Id</label>
-			<input type="text" name="customer_id" id="customer_id" value="1337"/>
+					<label for="customer_id">Customer Id</label>
+					<input type="text" name="customer_id" id="customer_id" value="1337"/>
 
-			<button name="Cancel">Cancel</button>
-			<button name="Install">Install</button>
-		</form>
+					<button name="Cancel" class="btn">Cancel</button>
+					<button name="Install" class="btn btn-success">Install</button>
+				</form>
 
-		<h2>Interactive App Code block installation - Pigeon Postal Service</h2>
+			</div>
+		</div>
+		<div class="panel panel-default">
+			<div class="panel-heading">
+				<h3 class="panel-title">
+					Interactive App Code block installation - Pigeon Postal Service
+				</h3>
+			</div>
+			<div class="panel-body">
 
-		<p>This will install the app after creating an interactive App Code block. In this example a postal service is installed. In the order management on the tab 'Connections' the merchant can choose
-		   different options when creating a package label.</p>
+				<p>This will install the app after creating an interactive App Code block. In this example a postal service is installed. In the
+				   order management on the tab 'Connections' the merchant can choose
+				   different options when creating a package label.</p>
 
-		<form action="Install.php" method="post">
-			<input type="hidden" name="api_public" id="api_public" value="<?= $_GET['api_public'] ?>"/>
-			<input type="hidden" name="install_type" id="install_type" value="postal_service"/>
+				<form action="Install.php" method="post">
+					<input type="hidden" name="api_public" id="api_public" value="<?= $_GET['api_public'] ?>"/>
+					<input type="hidden" name="install_type" id="install_type" value="postal_service"/>
 
-			<label for="customer_id">Customer Id</label>
-			<input type="text" name="customer_id" id="customer_id" value="1337"/>
+					<label for="customer_id">Customer Id</label>
+					<input type="text" name="customer_id" id="customer_id" value="1337"/>
 
-			<button name="Cancel">Cancel</button>
-			<button name="Install">Install</button>
-		</form>
+					<button name="Cancel" class="btn">Cancel</button>
+					<button name="Install" class="btn btn-success">Install</button>
+				</form>
+			</div>
+		</div>
 		</body>
 
 		</html>
