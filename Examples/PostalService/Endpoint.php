7289489ab8ddc205a989a92486a3ac692c40176d
Nick Postma <n.postma@ccvshop.nl>
2016-06-13 13:56:57 +0200

    Succes verandert in Success

--- Examples/PostalService/Endpoint.php
+++ Examples/PostalService/Endpoint.php
@@ -8,12 +8,12 @@
 		$oResponse = new \stdClass();
 		#Present 'Save As' Dialog for the merchant.
 		if($oObject->form_data->direct_download == '1') {
-			$oResponse->view                       = 'succes-direct-download';
+			$oResponse->view                       = 'success-direct-download';
 			$oResponse->data                       = [];
 			$oResponse->data['package_label_1']    = 'https://demo.securearea.eu/Examples/PostalService/Download.php?file=specimen_label.png';
 			$oResponse->data['attachment_label_1'] = 'https://demo.securearea.eu/Examples/PostalService/Download.php?file=specimen_label.png';
 		} else {
-			$oResponse->view                    = 'succes';
+			$oResponse->view                    = 'success';
 			$oResponse->data                    = [];
 			$oResponse->data['package_label_1'] = 'https://demo.securearea.eu/Examples/PostalService/Download.php?file=specimen_label.png';
 		}
